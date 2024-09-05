68cf9b173c0c7bae0cdd5e231f84121d2c269db1
sanju <sanjayjanardhan231@gmail.com>
2024-09-05 18:56:13 +0530

    added cart and your_orders

--- ORDER/controllers/orderController.js
+++ ORDER/controllers/orderController.js
@@ -1,4 +1,6 @@
 const food_order = require("../models/ordermodel");
+const your_orers = require("../models/yourorders");
+const cart = require("../models/cart");
 const jwt = require("jsonwebtoken");
 
 exports.order = async (req, res) => {
@@ -22,5 +24,59 @@ exports.order = async (req, res) => {
       success: false,
       error: err.message,
     });
+  }
+};
+
+exports.yourorders = async (req, res) => {
+  const { name, itemName, restaurant, price, createdAt, deliveryAt } = req.body;
+
+  const order = new your_orders({
+    name,
+    itemName,
+    restaurant,
+    price,
+    createdAt,
+    deliveryAt,
+  });
+
+  try {
+    const ordered = await order.save();
+    res.status(201).json({
+      success: true,
+      ordered,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+exports.cart = async (req, res) => {
+  try {
+    const { itemName, restaurant, price } = req.body;
+
+    const existingCartItem = await Cart.findOne({ itemName });
+
+    if (existingCartItem) {
+      return res.status(400).json({ message: "Item is already in the cart" });
+    }
+
+    const newCartItem = new Cart({
+      itemName,
+      restaurant,
+      price,
+      user: userId,
+    });
+
+    await newCartItem.save();
+
+    return res
+      .status(201)
+      .json({ message: "Item added to the cart", item: newCartItem });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
   }
 };
---
+++ ORDER/models/yourorders.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  itemName: {
+    type: String,
+    required: true,
+  },
+  deliveryAt: {
+    type: Date,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("YourOrder", orderSchema);
